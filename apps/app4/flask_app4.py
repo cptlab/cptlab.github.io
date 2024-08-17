ba5027383ffac4c727e2c6f49a68faff10e06a6b
smatthews <smatthews@cptldw00000.srv.sydney.edu.au>
2024-08-17 16:02:44 +1000

    Improve error handling M4

--- apps/app4/flask_app4.py
+++ apps/app4/flask_app4.py
@@ -16,17 +16,21 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    name = request.form['name']
-    image = run_prediction_model(name)
+    try:
+        name = request.form['name']
+        image = run_prediction_model(name)
 
-    # Save the image to a temporary file
-    image_filename = os.path.join(app.config['UPLOAD_FOLDER'], f"{name}.png")
-    image.save(image_filename)
+        # Save the image to a temporary file
+        image_filename = os.path.join(app.config['UPLOAD_FOLDER'], f"{name}.png")
+        image.save(image_filename)
 
-    # Generate the URL for the image
-    image_url = url_for('send_image', filename=f"{name}.png", _external=True)
+        # Generate the URL for the image
+        image_url = url_for('send_image', filename=f"{name}.png", _external=True)
 
-    return render_template('result.html', image_url=image_url)
+        return render_template('result.html', image_url=image_url)
+    except ValueError as e:
+        #Render error page.
+        return render_template('error.html', error_message=str(e)), 500
 
 @app.route('/Model4/static/temp/<filename>')
 def send_image(filename):
